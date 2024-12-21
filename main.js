373b998f40d3fe5b198662aae5fd433003a76250
Amens003 <121181740+Amens003@users.noreply.github.com>
2024-12-21 12:50:51 +0400

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const languageButton = document.getElementById('language');
 const encodingButton = document.getElementById('encoding');
 const decodingButton = document.getElementById('decoding');
 const resultArea = document.getElementById('result');
+const hackButton = document.getElementById('hack'); 
 
 function caesarCipher(text, shift, language) {
   let result = '';
@@ -24,6 +25,7 @@ function caesarCipher(text, shift, language) {
   return result;
 }
 
+
 encodingButton.addEventListener('click', () => {
   const text = textArea.value;
   const shift = parseInt(shiftButton.value);
@@ -36,4 +38,20 @@ decodingButton.addEventListener('click', () => {
   const shift = parseInt(shiftButton.value);
   const language = languageButton.value;
   resultArea.value = caesarCipher(text, -shift, language);
+});
+
+// Взлом зашифрованного текста пребором
+hackButton.addEventListener('click', () => {
+  const text = textArea.value;
+  const language = languageButton.value;
+  let hackResults = '';
+
+  const maxShift = language === 'en' ? 25 : 33;
+
+  for (let shift = 1; shift <= maxShift; shift++) {
+    const decodedText = caesarCipher(text, -shift, language);
+    hackResults += `Shift ${shift}: ${decodedText}\n`;
+  }
+
+  resultArea.value = hackResults;
 });
